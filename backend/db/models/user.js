46d764d1b2111d7e18e339b91511c2393df5fac8
germain <germaindev123@gmail.com>
2025-01-13 16:24:56 +0100

    Add db compliant method for user

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,9 +1,17 @@
 import {Channel} from "./channel";
 
 class User {
-    constructor(username, password, channelsCreated){
+    constructor(username, password, nickname){
         this.username = username;
         this.password = password;
-        this.channelsCreated = [];
+        this.nickname = nickname;
+    }
+
+    toConst() {
+        return {
+            username: this.username,
+            password: this.password,
+            nickname: this.nickname
+        }
     }
 }
