709d36229ba17147e8531d9510a3a9d92910a4c5
germain <germaindev123@gmail.com>
2025-01-14 15:42:40 +0100

    Update updateNickname in userServices.js

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -1,18 +1,38 @@
 import db from "../app";
 
-
+/**
+ * Creates a user in the database
+ * @param user
+ * @returns {InsertOneResult<Document>}
+ */
 export async function createUser(user) {
     return await db.collection("users").insertOne(user.toConst());
 }
 
+/**
+ * Gets a user from the database
+ * @param username the username of the user to get
+ * @returns {Document & {_id: InferIdType<Document>}}
+ */
 export async function getUser(username) {
-    return await db.collection("users").findOne({username: username});
+    return (await db.collection("users").findOne({username: username}));
 }
 
+/**
+ * Deletes a user from the database
+ * @param username
+ * @returns {DeleteResult}
+ */
 export async function deleteUser(username) {
     return await db.collection("users").deleteOne({username: username});
 }
 
-export async function updateUser(user) {
-    return await db.collection("users").updateOne({username: user.username}, {$set: user.toConst()});
+/**
+ * Updates a user in the database
+ * @param username {String} the user to update
+ * @param nickname {String} the new nickname
+ * @returns {UpdateResult<Document>}
+ */
+export async function updateNickname(username, nickname) {
+    return await db.collection("users").updateOne({username: username}, {$set: {nickname: nickname}});
 }
