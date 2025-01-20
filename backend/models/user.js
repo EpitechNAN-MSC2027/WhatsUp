7bc0a0418548a495313a6464e60038cd04366ee0
germain <germaindev123@gmail.com>
2025-01-20 10:07:57 +0100

    Update user and userServices.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,23 @@
 export class User {
-    constructor(username, password, nickname){
+    /**
+     * @param {String} username
+     * @param {String} password
+     * @param {String} nickname
+     * @param {String[]} channels
+     */
+    constructor(username, password, nickname, channels){
         this.username = username;
         this.password = password;
         this.nickname = nickname;
+        this.channels = channels;
     }
 
     toConst() {
         return {
             username: this.username,
             password: this.password,
-            nickname: this.nickname
+            nickname: this.nickname,
+            channels: this.channels
         }
     }
 }
