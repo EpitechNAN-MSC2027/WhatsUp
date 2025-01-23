604d02d6247915d2c92f85b3ae117b912065a9ab
belli <bellinna.uong@outlook.com>
2025-01-23 23:25:15 +0100

    show the real name of the username in rightside

--- frontend/src/chat/sidebar-right/RightSidebar.jsx
+++ frontend/src/chat/sidebar-right/RightSidebar.jsx
@@ -4,12 +4,19 @@ import ProfileSection from './profile';
 import MembersSection from './members';
 import avatarGif from './avatar.gif';
 
-const RightSidebar = ({ profile, members }) => (
-    <div className="right-sidebar">
-        <ProfileSection name={profile.name}/>
-        <img src={avatarGif} alt="Divider GIF" class={"avatar"}/>
-        <MembersSection members={members}/>
-    </div>
-);
+const RightSidebar = ({ profile, members }) => {
+    const username = localStorage.getItem('username');
+    
+    return (
+        <div className="right-sidebar">
+            <ProfileSection 
+                username={username || 'Utilisateur'} 
+                name={profile?.name}
+            />
+            <img src={avatarGif} alt="Divider GIF" className="avatar"/>
+            <MembersSection members={members}/>
+        </div>
+    );
+};
 
 export default RightSidebar;
