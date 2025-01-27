e0a46391b4b2d428fa57efc5c26ee779a26f870a
belli <bellinna.uong@outlook.com>
2025-01-27 21:52:05 +0100

    keeping history of messages

--- frontend/src/chat/middle/chatWindow.jsx
+++ frontend/src/chat/middle/chatWindow.jsx
@@ -20,23 +20,28 @@ const ChatWindow = ({ currentChannel, socket }) => {
     useEffect(() => {
         if (!socket) return;
 
-        /*
         socket.on('channel', (channel) => {
             console.log('Current channel:', channel);
-            currentChannel = channel;
         });
-         */
 
         socket.on('history', (history) => {
-            console.log('messages historic:', history);
-            setMessages(history);
-            console.log('messages array:', messages);
+            console.log('Messages history received:', history);
+            const formattedHistory = history.map(msg => ({
+                text: msg,
+                sender: msg.split(':')[0],
+                message: msg.split(':')[1],
+                type: 'received'
+            }));
+            setMessages(formattedHistory);
         });
 
         socket.on('message', (messageData) => {
             console.log('Message reçu:', messageData);
             setMessages(prevMessages => [...prevMessages, {
                 text: messageData,
+                sender: messageData.split(':')[0],
+                message: messageData.split(':')[1],
+                type: 'received'
             }]);
         });
 
@@ -44,7 +49,7 @@ const ChatWindow = ({ currentChannel, socket }) => {
             console.log('Response from server:', response);
             
             if (response.action === 'join' && response.status === 'success') {
-                //setMessages([]); // Réinitialiser les messages lors du changement de canal
+                // L'historique sera reçu via l'événement 'history'
             } else if (response.action === 'list' && response.status === 'success') {
                 setChannels(response.data);
                 setMessages(prev => [...prev, {
@@ -58,6 +63,8 @@ const ChatWindow = ({ currentChannel, socket }) => {
         return () => {
             socket.off('message');
             socket.off('response');
+            socket.off('history');
+            socket.off('channel');
         };
     }, [socket]);
 
@@ -141,8 +148,8 @@ const ChatWindow = ({ currentChannel, socket }) => {
                             </div>
                         ) : msg.sender ? (
                             <>
-                                <span className="message-sender">{msg.sender}: </span>
-                                <span className="message-text">{msg.text}</span>
+                                <span className="message-sender">{msg.sender} : </span>
+                                <span className="message-text">{msg.message}</span>
                             </>
                         ) : (
                             <span className="message-text">{msg.text}</span>
