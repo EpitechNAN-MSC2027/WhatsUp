c4bf963cfab0b696eef0aaaf5e10ceaa278625fc
germain <germaindev123@gmail.com>
2025-01-27 14:37:27 +0100

    Add vitest

--- backend/db.test.mjs
+++ backend/db.test.mjs
@@ -1,8 +1,13 @@
+
+import { expect, test } from 'vitest'
+import db from "db/connection.js";
+
+
 test('adds 1 + 2 to equal 3', () => {
     expect(true).toBe(true);
 });
 
 test('should get all channels', async () => {
-    let channels = dbTest.collection("channels").find().toArray();
+    let channels = await db.collection("channels").find().toArray();
     expect(channels.length).toBe(0);
 });
