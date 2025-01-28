2c56c97b503441bf1f6976dda232f80e783c7fbf
belli <bellinna.uong@outlook.com>
2025-01-28 11:21:29 +0100

    back for the notif

--- backend/controller/handleCommands.js
+++ backend/controller/handleCommands.js
@@ -269,6 +269,8 @@ export async function joinChannel(socket, channel) {
 
         await connectChannel(socket, channel);
 
+        socket.to(channel).emit('userJoined', socket.user.nickname || socket.user.username);
+
         await successResponse(
             socket,
             'join',
