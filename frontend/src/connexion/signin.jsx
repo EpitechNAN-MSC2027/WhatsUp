1d8e5d5f436841e85dbaac498a1c9cf04678323e
LeoPartt <guerizec.l@gmail.com>
2025-01-31 20:14:00 +0100

    Add dynamic URL handling

--- frontend/src/connexion/signin.jsx
+++ frontend/src/connexion/signin.jsx
@@ -9,7 +9,7 @@ const SignInForm = ({ switchToSignUp, navigateToChat }) => {
         const password = formData.get('password');
 
         try {
-            const response = await fetch('http://localhost:3000/login', {
+            const response = await fetch(`http://${window.location.hostname}:3000/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
